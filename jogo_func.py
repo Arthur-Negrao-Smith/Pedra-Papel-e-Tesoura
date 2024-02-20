998942d35af0c050685255b5fd21e319a8a509db
Arthur Negrão <arthurnegrao10s@gmail.com>
2024-02-20 20:08:43 -0300

    Initial commit

---
+++ jogo_func.py
@@ -0,0 +1,62 @@
+from random import randint
+
+class Jogo:
+    def __init__(self) -> None:
+        self.escolha_pes = int
+        self.escolha_pc = int
+
+    def tela_titulo(self):
+        titulo = 'Pedra, Papel e tesoura'
+        print(f"{titulo:-^50}")
+
+    def escolher_pc(self) -> int:
+        """Serve para escolher um número para o sistema"""
+        self.escolha_pc = randint(0, 2)
+        return self.escolha_pc
+    
+    def tela_resultado(self, escolha_pes, escolha_pc):
+        if escolha_pes == 0 and escolha_pc == 2 or escolha_pes == 1 and escolha_pc == 0 or escolha_pes == 2 and escolha_pc == 1:
+            print('PARABÉNS! \nVOCÊ GANHOU... Pura sorte...')
+        elif escolha_pes == escolha_pc:
+            print('DEU EMPATE! Maldito humano...')
+        else:
+            print('Não foi dessa vez HUMANO! HUAHUAHUA')
+        
+    def continuar(self):
+        try:
+            voltar = input('Deseja continuar? [s/n] ').strip()
+            continua = False
+            if voltar in '':
+                print('Valor inválido')
+                continua = True
+                return True, continua
+            elif voltar in 'snSN':
+                if voltar in 'Ss':
+                    print('CARREGANDO...')
+                    continua = True
+                    return False, continua
+                else:
+                    print('Fechando jogo...')
+                    continua = False
+                    return False, continua
+            else:
+                continua = True 
+                return True, continua
+        except:
+            print('Valor inválido')
+            return True, continua
+
+    def perguntar_escolha(self):
+        """Serve para perguntar Qual a opção do player"""
+        print(f'Pedra[ 0 ] \nPapel [ 1 ] \nTesoura[ 2 ] ')
+        while True:
+            try:
+                self.escolha_pes = int(input('Sua escolha: '))
+                if self.escolha_pes not in [0, 1, 2]:
+                    print('valor inválido')
+                else:
+                    break
+            except:
+                print('Valor inválido')
+        return self.escolha_pes
+
---
+++ main.py
@@ -0,0 +1,22 @@
+from jogo_func import *
+
+# Inicia o Jogo
+jogo = Jogo()
+
+# Cria a lista de opções
+possibilidades = ['Pedra', 'Papel', 'Tesoura']
+
+# Loop do jogo
+while True:
+    loop = True
+    jogo.tela_titulo()
+    resposta_pes = jogo.perguntar_escolha()
+    resposta_pc = jogo.escolher_pc()
+    print(f'Escolha o Humano: {possibilidades[resposta_pes]}\nEscolha da Máquina {possibilidades[resposta_pc]}')
+    resultado = jogo.tela_resultado(escolha_pc=resposta_pc, escolha_pes=resposta_pes)
+    while loop:
+        loop, continuar = jogo.continuar()
+    if continuar:
+        continue
+    else:
+        break
